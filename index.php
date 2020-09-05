c0cb373e5c57f97c137c34f29c27267815a113f8
ftp <godzila_ssh@iohost.beget.ru>
2020-09-06 02:25:12 +0300

    check deploy test

--- index.php
+++ index.php
@@ -1,15 +1,1 @@
-﻿<?
-require_once($_SERVER['DOCUMENT_ROOT'].'/app/prolog.php');
-
-$product = new Product();
-$arResult = $product->find('all',
-    [
-        'order' => 'sort asc'
-    ]
-);
-?>
-<section class="content">
-    <?=General::renderTemplate('/templates/product/product_list.html',['products' => $arResult]);?>
-</section>
-<?
-require_once($_SERVER['DOCUMENT_ROOT'].'/app/epilog.php');
+﻿<?='test'?>
