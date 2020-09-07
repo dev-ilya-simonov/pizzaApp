febf1aa7a47715809aef169f606ec3543a51cd3a
GodZiLA <godzila_ssh@iohost.beget.ru>
2020-09-07 12:30:48 +0300

    smth wrong

--- app/ajax/createBasket.php
+++ app/ajax/createBasket.php
@@ -3,7 +3,7 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/app/prolog_before.php');
 
 $basket = new Basket;
 $tempBasket = $basket->getTemp();
-
+d($tempBasket);/*
 $basket->items = json_encode($tempBasket['items']);
 $basket->total = json_encode($tempBasket['total']);
 $basket->date  = date('d.m.Y H:i:s');
@@ -13,3 +13,4 @@ if($newID = $basket->save()) {
     echo $newID;
 } else
     echo 'Ups! it seems that something went wrong... Please, try again later!';
+*/
