d2383fa940c20a770bbf6ad3fa66042dd3574153
GodZiLA <godzila_ssh@iohost.beget.ru>
2020-09-07 12:34:01 +0300

    hope fixed

--- app/ajax/createBasket.php
+++ app/ajax/createBasket.php
@@ -3,14 +3,12 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/app/prolog_before.php');
 
 $basket = new Basket;
 $tempBasket = $basket->getTemp();
-d($tempBasket);/*
+
 $basket->items = json_encode($tempBasket['items']);
 $basket->total = json_encode($tempBasket['total']);
-$basket->date  = date('d.m.Y H:i:s');
 
 if($newID = $basket->save()) {
     setcookie("basket", '', time()-3600*3,'/');  // cookie set to 3 hours
     echo $newID;
 } else
     echo 'Ups! it seems that something went wrong... Please, try again later!';
-*/
