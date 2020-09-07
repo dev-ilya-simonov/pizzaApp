914e29c88c52971d6c07614f82893a19445c3e15
GodZiLA <godzila_ssh@iohost.beget.ru>
2020-09-07 13:37:17 +0300

    db fix

--- app/ajax/createBasket.php
+++ app/ajax/createBasket.php
@@ -6,10 +6,9 @@ $tempBasket = $basket->getTemp();
 
 $basket->items = json_encode($tempBasket['items']);
 $basket->total = $tempBasket['total'];
-/*
+
 if($newID = $basket->save()) {
     setcookie("basket", '', time()-3600*3,'/');  // cookie set to 3 hours
     echo $newID;
 } else
     echo 'Ups! it seems that something went wrong... Please, try again later!';
-*/
